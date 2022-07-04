53537925c2e2977a7da2fd37125c900ca47c42b1
Dragusin Mihai <chaotikdarkness@gmail.com>
2022-07-04 22:26:00 +0800

    basic router and config from json

--- main.cpp
+++ main.cpp
@@ -11,52 +11,68 @@
 #include <vector>
 #include <map>
 #include "router.cpp"
+#include "config.cpp"
 
 int SERVER_PORT;
 
-class RequestHandler : public Poco::Net::HTTPRequestHandler {
-    public:
-
-    virtual void handleRequest(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) {
+class RequestHandler : public Poco::Net::HTTPRequestHandler
+{
+public:
+    virtual void handleRequest(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res)
+    {
         const std::string uri = req.getURI();
         const std::string method = req.getMethod();
-        std::cout 
-        << method << " "
-        << uri << " "
-        << req.clientAddress()
-        << std::endl;
+        std::cout
+            << method << " "
+            << uri << " "
+            << req.clientAddress()
+            << std::endl;
 
         router(req, res, uri, method);
     };
     static int count;
 };
 
-
-class HandlerFactory : public Poco::Net::HTTPRequestHandlerFactory {
-    public:
-    virtual Poco::Net::HTTPRequestHandler* createRequestHandler(const Poco::Net::HTTPServerRequest &) {
+class HandlerFactory : public Poco::Net::HTTPRequestHandlerFactory
+{
+public:
+    virtual Poco::Net::HTTPRequestHandler *createRequestHandler(const Poco::Net::HTTPServerRequest &)
+    {
         return new RequestHandler;
     };
 };
 
-
-class Server : public Poco::Util::ServerApplication {
-    protected:
-    int main(const std::vector<std::string> &args) {
-            Poco::Net::HTTPServer s(new HandlerFactory, Poco::Net::ServerSocket(SERVER_PORT), new Poco::Net::HTTPServerParams);
-            s.start();
-            Poco::Util::ServerApplication::waitForTerminationRequest();
-            s.stop();
+class Server : public Poco::Util::ServerApplication
+{
+protected:
+    int main(const std::vector<std::string> &args)
+    {
+        Poco::Net::HTTPServer s(new HandlerFactory, Poco::Net::ServerSocket(SERVER_PORT), new Poco::Net::HTTPServerParams);
+        s.start();
+        Poco::Util::ServerApplication::waitForTerminationRequest();
+        s.stop();
     };
 };
 
-int main(int n, char** args) {
-    if (n < 2) {
+int main(int n, char **args)
+{
+    Poco::JSON::Object::Ptr config = getConfig();
+    std::string dbName = config->get("dbName").toString();
+    std::string dbHost = config->get("dbHost").toString();
+    std::string dbPort = config->get("dbPort").toString();
+    std::string dbUser = config->get("dbUser").toString();
+    std::string dbPass = config->get("dbPass").toString();
+    std::string serverPort = config->get("serverPort").toString();
+
+    if (n < 2)
+    {
         std::cout << "server port required as argument" << std::endl;
         return 1;
-    } else {
-        SERVER_PORT = std::stoi(args[1]);
+    }
+    else
+    {
+        SERVER_PORT = std::stoi(serverPort);
     }
     Server s;
-    return s.run(n,args);
+    return s.run(n, args);
 }
--- router.cpp
+++ router.cpp
@@ -9,27 +9,36 @@
 #define HTTP_PATCH "PATCH"
 #define HTTP_DELETE "DELETE"
 
-void handleIndex(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) {
-        res.setStatus(Poco::Net::HTTPResponse::HTTP_OK);    
-        res.setContentType("text/html");
-        std::ostream& wr = res.send();
-        wr << "<h3>WolvenSpirit</h3>" << std::endl;
-        wr.flush();
-        return;
-    }
-    void handleGetData(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res) {
+void handleIndex(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res)
+{
+    res.setStatus(Poco::Net::HTTPResponse::HTTP_OK);
+    res.setContentType("text/html");
+    std::ostream &wr = res.send();
+    wr << "<h3>WolvenSpirit</h3>" << std::endl;
+    wr.flush();
+    return;
+}
+void handleGetData(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res)
+{
+}
 
+void router(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res, const std::string uri, const std::string method)
+{
+    if (uri == "/" && method == HTTP_GET)
+    {
+        handleIndex(req, res);
     }
-
-void router (Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &res, const std::string uri, const std::string method) {
-        if (uri == "/" && method == HTTP_GET) { handleIndex(req,res);}
-        else if (uri == "/data" && method == HTTP_GET) { handleGetData(req,res);}
-        else {
-        res.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_FOUND);    
+    else if (uri == "/data" && method == HTTP_GET)
+    {
+        handleGetData(req, res);
+    }
+    else
+    {
+        res.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_FOUND);
         res.setContentType("text/html");
-        std::ostream& wr = res.send();
+        std::ostream &wr = res.send();
         wr << "<h3>We can't seem to find what you are looking for.</h3>" << std::endl;
         wr.flush();
         return;
-        }
+    }
 }
