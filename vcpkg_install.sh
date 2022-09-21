aa947398c10fdb28f56df384e22dea0a9a7abbe4
Dragusin Mihai <chaotikdarkness@gmail.com>
2022-09-21 13:02:32 +0800

    install resource-pool

--- vcpkg_install.sh
+++ vcpkg_install.sh
@@ -7,3 +7,8 @@ git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/vcpkg install libpqxx
 ./vcpkg/vcpkg install prometheus-cpp
 ./vcpkg/vcpkg install catch2:x64-linux
+
+# Install custom lib that is not tracked in vcpkg
+git clone https://github.com/WolvenSpirit/resource-pool
+ln -s ${PWD}/resource-pool/resource-pool vcpkg/ports/resource-pool
+./vcpkg/vcpkg install resource-pool:x64-linux
